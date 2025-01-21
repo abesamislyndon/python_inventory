2fe6eff8ec9b21ad907945f666c5e82582700c40
dondon <abesamislyndon@gmail.com>
2025-01-21 16:51:12 +0800

    1. add query for dashboard data for events count, event post, current date post 2. fix events table list proper tailwind table 3. refactor design on login page

--- inven/app/dashboards/views.py
+++ inven/app/dashboards/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from ..clients.models import Client, ClientMsg
 
 def dashboard_home(request):
-    return render(request, 'dashboards/dashboard.html') 
+    
+    event_count = Client.objects.count()
+    msg_count = ClientMsg.objects.count()
+    today = timezone.now().date()
+    event_count_today = Client.objects.filter(created_at=today).count()
+   
+
+    return render(request, 'dashboards/dashboard.html', {'event_count': event_count, "msg_count": msg_count, "event_count_today": event_count_today}) 
